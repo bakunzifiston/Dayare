fcb26b6b4c9b76369252dd9bd82a002ab7a70279
bakunzifiston <82505728+bakunzifiston@users.noreply.github.com>
2026-04-12 16:53:05 +0200

    p

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    /*
+     * Registered explicitly so Swagger UI (/api/documentation) works on hosts where
+     * package auto-discovery does not run (e.g. some cPanel / optimized deploys).
+     */
+    L5Swagger\L5SwaggerServiceProvider::class,
 ];
